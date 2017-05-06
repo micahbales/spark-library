6817c4849df45ba3bb41c4044138b398339c30ff
Micah Bales <micahbales@gmail.com>
2017-05-05 22:43:19 -0400

    create Book model

---
+++ app/models/book.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  author: DS.attr('string'),
+  description: DS.attr('string'),
+  url: DS.attr('string'),
+  favorite: DS.attr('boolean'),
+  createdAt: DS.attr('date', {
+    defaultValue() { return new Date(); }
+  })
+});
