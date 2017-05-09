e67c17edeb300762ffa8dd26e942574676d870c3
Micah Bales <micahbales@gmail.com>
2017-05-09 13:30:25 -0400

    highlight current page in primary-nav-bar

--- app/components/primary-nav-bar.js
+++ app/components/primary-nav-bar.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement() {
+    Ember.$('.primary-nav-bar .navbar-nav li').on('click', ({currentTarget} /* e.currentTarget */) => {
+      /* Arrow functions do not bind `this`, so we have to pass currentTarget
+        from the click event in order to know which element to target */
+      Ember.$('.primary-nav-bar .navbar-nav li.active').removeClass('active');
+      Ember.$(currentTarget).addClass('active');
+    });
+
+    Ember.$('.navbar-brand').on('click', ({currentTarget}) => {
+      Ember.$('.primary-nav-bar .navbar-nav li.active').removeClass('active');
+      Ember.$('.primary-nav-bar .navbar-nav li').first().addClass('active');
+    })
+  }
 });
